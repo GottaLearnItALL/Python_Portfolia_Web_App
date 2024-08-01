db0d7ffcdcb6574806c576d073d07f668632a70d
Aryan Bhat <aryanbhat.2017@gmail.com>
2024-08-01 13:57:44 +0530

    Added description to a new columns

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/21.jpg", width=380)
+    st.image("images/21.jpg", width=480)
 
 with col2:
     st.title("Aryan Bhat")
@@ -13,3 +13,11 @@ with col2:
     Hi I am Aryan!, I am upcoming python programmer, and student at Rutgers University.
     """
     st.info(content)
+
+content2 = """
+            Below you can find some of the apps I have built. Feel Free to contact me!!
+            """
+st.text(content2)
+
+
+col3, col4 = st.columns(2)
