54a3a53b02e7253f8a59d1056e8146a7c31e750d
Aryan Bhat <aryanbhat.2017@gmail.com>
2024-08-01 14:39:11 +0530

    add app description, images and links to the web app

--- main.py
+++ main.py
@@ -21,14 +21,19 @@ content2 = """
 st.text(content2)
 
 
-col3, col4 = st.columns(2)
+col3, col5, col4 = st.columns([1.5, 0.5, 1.5])
 
 
 data = pandas.read_csv("data.csv",sep=";")
 with col3:
     for index, row in data[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in data[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
