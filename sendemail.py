7ba1281061156bc502f22f31d9b08bf738201160
Aryan Bhat <aryanbhat.2017@gmail.com>
2024-08-09 16:55:38 +0530

    added my project links to the csv file

--- sendemail.py
+++ sendemail.py
@@ -2,16 +2,17 @@ import smtplib, ssl
 import os
 
 
-def send_email(receiver, message):
+def send_email(message,receiver):
     host = "smtp.gmail.com"
     port = 465
+    #receiver = "iamkami.2023@gmail.com"
     username = "iamkami.2023@gmail.com"
     password = os.getenv("PASSWORD")
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
         server.login(username, password)
-        server.sendmail(username, receiver, message)
+        server.sendmail(username,receiver,message)
 
 
 
